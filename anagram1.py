3439f5ff37970a16d95b94059c6978df3d18b1df
ko22yu <kikirara.rainbow@gmail.com>
2021-05-13 21:26:52 +0900

    ファイルの読み込み, better_solution関数を修正

--- anagram1.py
+++ anagram1.py
@@ -14,22 +14,29 @@ def binary_search(lst, target):
 
 
 def better_solution(random_word, dictionary):
-    for i, words in enumerate(new_dictionary):
-        new_dictionary[i] = words.split()
+    new_dictionary = []
+    for words in dictionary:
+        new_dictionary.append(words.split())
 
     sorted_random_word = ''.join(sorted(random_word))  # random_wordの単語自身をソートしたもの
     anagram = binary_search(new_dictionary, sorted_random_word)
     return anagram
 
 
-print("文字列を入力してください(英語)")
-str = input()
+def main():
+    print("文字列を入力してください(英語)")
+    str = input()
+    
+    # 事前にソートした辞書を読み込む
+    try:
+        f = open("sort_words.txt", "r") 
+        new_dictionary = f.readlines()
+    finally:
+        f.close()
 
-# 事前にソートした辞書を読み込む
-f = open("sort_words.txt", "r") 
-new_dictionary = f.readlines()
-f.close()
-
-print(better_solution(str, new_dictionary))
+    print(better_solution(str, new_dictionary))
 
 
+
+if __name__ == '__main__':
+    main()
